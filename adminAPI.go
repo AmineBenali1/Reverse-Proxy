7333f130845f155a485281af4caeb6912c8b4d99
Amine Benali <amine.benali@um6p.ma>
2026-01-18 14:49:37 +0000

    delete backend function

--- adminAPI.go
+++ adminAPI.go
@@ -49,9 +49,30 @@ func addBackend(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	var newBackend *Backend
-	json.NewDecoder(r.Body).Decode(&newBackend)
+	if err := json.NewDecoder(r.Body).Decode(&newBackend); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
 	serverPool.Backends = append(serverPool.Backends, newBackend)
 	w.Write([]byte("Backend added"))
 }
 
-
+func deleteBackend(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/backends" {
+		http.Error(w, "Path not found", http.StatusNotFound)
+		return
+	}
+	var backendToDelete Backend
+	if err := json.NewDecoder(r.Body).Decode(&backendToDelete); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	for i, backend := range serverPool.Backends {
+		if backend.URL == backendToDelete.URL {
+			serverPool.Backends = append(serverPool.Backends[:i], serverPool.Backends[i+1:]...)
+			w.Write([]byte("Backend Deleted Successfully"))
+			return
+		}
+	}
+	http.Error(w, "Backend not found", http.StatusNotFound)
+}
